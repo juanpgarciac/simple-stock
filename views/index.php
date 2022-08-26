947207968c343e80d1afbe842aa7e3d1ff85cfaf
JPG <jpg.soporte@gmail.com>
2022-08-25 20:48:14 -0400

    add bootstrap icons

--- views/index.php
+++ views/index.php
@@ -1,9 +1,9 @@
 <h1>Home</h1>
     <div class="card" style="width: 18rem;">
-    <img src="https://picsum.photos/200" class="card-img-top" alt="...">
+    <img src="/img/product.png" class="card-img-top" alt="Simple stock">
     <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <h5 class="card-title">Simple Stock</h5>
+        <p class="card-text">Simple stock application. Made with <i class="bi bi-heart-fill" style="color:red"></i> with PHP & Bootstrap</p>
         <a href="/product" class="btn btn-primary">Products Index</a>
     </div>
     </div>
