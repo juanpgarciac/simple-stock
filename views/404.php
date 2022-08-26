c1d5f5750a73863b9e7fb43bdd4647ef21261f48
JPG <jpg.soporte@gmail.com>
2022-08-26 09:49:40 -0400

    Give more style to views

--- views/404.php
+++ views/404.php
@@ -1,1 +1,7 @@
-<h1>Sorry, not found!</h1>
+<?php /** @layout layouts.main */ ?>
+<div class="row">
+    <div class="col">
+        <h1>Error 404 <i class="bi bi-exclamation-circle"></i></h1>
+        <h2>Sorry, the page was not found!</h2>
+    </div>
+</div>
